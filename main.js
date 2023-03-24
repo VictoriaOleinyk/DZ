f02fd2042f386b5b0953f55f40c350491c15e6d9
VictoriaOleinyk <vikacrimea@yandex.ru>
2023-03-24 22:13:01 +0600

    hw_4

--- main.js
+++ main.js
@@ -1,41 +1,31 @@
-const box = document.querySelector('.box')
-
-let positionX = 0
-let positionY = 0
-
-const move = () => {
-    if (positionX <= 449 && positionY === 0) {
-        positionX++
-        box.style.left = `${positionX}px`
-        setTimeout(move, 1)
-    } else if (positionX >= 449 && positionY <= 449){
-        positionY++
-        box.style.top = `${positionY}px`
-        setTimeout(move, 1)
-    } else if (positionY >= 449 && positionX >= 1){
-        positionX--
-        box.style.left = `${positionX}px`
-        setTimeout(move, 1)
+const btn = document.querySelector('.btn')
+const wrapper = document.querySelector('.wrapper')
+wrapper.innerHTML = ""
+btn.addEventListener('click', () => {
+    if (  wrapper.innerHTML == "") {
+        const request = new XMLHttpRequest()
+        request.open("GET", "peoples.json")
+        request.setRequestHeader("Content-type", "application/json")
+        request.send()
+        request.addEventListener('load', () => {
+            const peoples = JSON.parse(request.response)
+            peoples.forEach((people) => {
+                const div = document.createElement('div')
+                div.setAttribute('class', 'user')
+                div.innerHTML = `
+    <img src="${people.url}" alt="picture">
+    <h2>Name: ${people.name}</h2>
+    <span>Age: ${people.age} </span>
+    `
+                wrapper.append(div)
+            })
+        })
+    } else {
+        wrapper.innerHTML = ""
     }
-    else if (positionX === 0 && positionY <= 450){
-        positionY--
-        box.style.top = `${positionY}px`
-        setTimeout(move, 1)
-    }
-}
-move()
 
-let c = 0
-let p
 
-function start() {
-    c++;
-    document.getElementById('counter').innerHTML = c;
-}
-function fun1() {
-    p = setInterval('start()', 600);
-}
-function fun2() {
- clearInterval(p)
-}
+} )
+
+
 
