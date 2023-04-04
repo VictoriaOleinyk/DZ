b6da0138690bd34238fa09182dd5f126cc9f5976
VictoriaOleinyk <vikacrimea@yandex.ru>
2023-04-04 22:00:25 +0600

    hw_7

--- main.js
+++ main.js
@@ -1,51 +1,39 @@
+const cards = document.querySelector('.cards')
+const urlImg = 'https://robb.report/upload/img_cache/4a0/4a0f20ad62a962c3214cd9e267365d20_ce_268x197x0x0_cropped_800x427.jpg'
+const url = 'https://jsonplaceholder.typicode.com/posts?userId=1&&userId=2'
 
-const block = document.querySelector('.block')
-const btnPrev = document.querySelector('.prev')
-const btnNext = document.querySelector('.next')
-
-let count = 1
-
-function disableButtons (){
-    btnPrev.disabled = count <= 1
-    btnNext.disabled = count >= 200
+const delay = (ms) => {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve()
+        }, ms)
+    })
 }
 
-const cardText = () => {
-    fetch(`https://jsonplaceholder.typicode.com/todos/${count}`)
-        .then(response => response.json())
-        .then (data => {
-            block.innerHTML = `
-            <h2>${data.title}/</h2>
-            <span>${data.id}</span>
-            <h3>${data.completed}</h3>
-            `
-})
-}
+const fetchData = async () => {
+    try {
+        await delay(1000)
+        const resp = await fetch(url)
+        const data = await resp.json()
+        data.forEach((object) => {
+            const div = document.createElement('div')
+            div.setAttribute('class', 'text')
+            div.innerHTML = `
+                <img src="${urlImg}" alt="picture">
+                <h3>${object.title}</h3>
+                <p>${object.body} </p>
+    `
+                cards.append(div)
 
-function cardChange () {
-    const div = document.querySelector('div');
-    div.addEventListener('click', (event) => {
-    if (event.target.classList.contains('next')){
-        count++;
-        disableButtons()
-        cardText();
-    } else {
-        count--;
-        disableButtons()
-        cardText()
+        })
+    } catch {
+        console.error('ERROR')
     }
-    })
+    finally {
+        console.warn('DONE')
+    }
 }
-
-cardChange()
-
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    })
-
-
+fetchData()
 
 
 
